71e452e28c93c655b04d29d30a193ece3a7023ab
Leon Liu <liuleon360@gmail.com>
2024-10-18 15:20:45 -0700

    Add statement evaluation, setting up main, executing main

--- README.md
+++ README.md
@@ -29,3 +29,4 @@ You can find out more about our autograder, including how to run it, in [the acc
 This is an unlicensed repository; even though the source code is public, it is **not** governed by an open-source license.
 
 This code was primarily written by [Carey Nachenberg](http://careynachenberg.weebly.com/), with support from his TAs for the [Fall 2024 iteration of CS 131](https://ucla-cs-131.github.io/fall-24-website/).
+
--- interpreterv1.py
+++ interpreterv1.py
@@ -1,5 +1,6 @@
-from interpreterbase import InterpreterBase
-from brewparse import parse_program, ErrorType, Element
+from intbase import InterpreterBase, ErrorType
+from brewparse import parse_program
+from element import Element
 
 from typing import Optional, List, Dict, Any
 
@@ -35,18 +36,31 @@ class Interpreter(InterpreterBase):
     def __init__(self, console_output=True, inp=None, trace_output=False):
         super().__init__(console_output, inp) 
         
-        self.main_scope = Scope()
+        self.main_scope = Scope(interpreter=self)
         
     def run(self, program: str):
         ast = parse_program(program)
+        program_node = ast
+        # root node should be program node
+        assert(program_node.elem_type == InterpreterBase.PROGRAM_NODE)
         
-    def setup_main_scope(self):
+        # add functions under program node to scope
+        self.setup_main_scope(program_node.get("functions"))
+        
+        # call the main function
+        self.main_scope.functions["main"].execute(self.main_scope, [])
+        
+    def setup_main_scope(self, funcs: List[Element]):
         """
         Add built-in functions to the main scope
         """
-        self.main_scope.functions["print"] = PrintFunction()
-        self.main_scope.functions["input"] = InputFunction(super().get_input)
+        self.main_scope.functions["print"] = PrintFunction(self)
+        self.main_scope.functions["input"] = InputFunction(self)
         
+        for func in funcs:
+            assert(func.elem_type == InterpreterBase.FUNC_NODE)
+            
+            self.main_scope.functions[func.get("name")] = Function(self, func)
         
 class Scope():
     '''
@@ -60,8 +74,8 @@ class Scope():
         
         self.parent = parent
         
-    def add_variable(self, name, value):
-        self.variables[name] = Variable(value)
+    def add_variable(self, name):
+        self.variables[name] = Variable(interpreter=self.interpreter)
         
     def assign_variables(self, name, value):
         if self.check_variable(name):
@@ -69,7 +83,7 @@ class Scope():
         elif self.parent:
             self.parent.assign_variables(name, value)
         else:
-            self.interpreter.raise_error(ErrorType.NAME_ERROR, f"Variable {name} not found")
+            self.interpreter.error(ErrorType.NAME_ERROR, f"Variable {name} not found")
             
     def get_variable(self, name):
         if self.check_variable(name):
@@ -77,7 +91,7 @@ class Scope():
         elif self.parent:
             return self.parent.get_variable(name)
         else:
-            self.interpreter.raise_error(ErrorType.NAME_ERROR, f"Variable {name} not found")
+            self.interpreter.error(ErrorType.NAME_ERROR, f"Variable {name} not found")
         
     def check_variable(self, name, recursive=False):
         if name in self.variables:
@@ -114,8 +128,8 @@ class Function():
         self.name = function_node.get("name")
         self.statements = function_node.get("statements")
         
-    def execute(self, calling_scope: Optional[Scope], args: Optional[List[Any]]):
-        fcall = FunctionCall(self.name, self, [], calling_scope)
+    def execute(self, calling_scope: Optional[Scope], args: Optional[List[Any]]=None):
+        fcall = FunctionCall(self.interpreter, self.name, self, args, calling_scope)
         fcall.run()
         
 class PrintFunction(Function):
@@ -142,7 +156,7 @@ class PrintFunction(Function):
                 values.append(arg.get("value"))
             else:
                 # raise error if the arg is not a variable or value
-                self.__annotations__interpreter.raise_error(ErrorType.TYPE_ERROR, f"Invalid argument type {arg.elem_type}")
+                self.interpreter.error(ErrorType.TYPE_ERROR, f"Invalid argument type {arg.elem_type}")
         super().output(values)
         
 class InputFunction(Function):
@@ -183,49 +197,79 @@ class FunctionCall():
         self.scope = Scope(calling_scope)
         
     def run(self):
-        pass # TODO execute statements
+        # execute list of statement nodes in function node
+        statements = self.function.function_node.get("statements")
+        for statement in statements:
+            self.evaluate_statement(statement)
     
     def evaluate_statement(self, statement: Element):
         # Check that statement is a valid statement
-        assert(statement.elem_type in InterpreterBase.VALID_STATEMENTS)
+        assert(statement.elem_type in Interpreter.STATEMENT_NODES)
+        
+        match (statement.elem_type):
+            case InterpreterBase.VAR_DEF_NODE:
+                # add the variable to the scope
+                self.scope.add_variable(statement.get("name"))
+            case Interpreter.ASSIGN_NODE:
+                # assign the variable
+                self.scope.assign_variables(statement.get("name"), self.evaluate_expression(statement.get("expression")))
+            case InterpreterBase.FCALL_NODE:
+                # evaluate the function call
+                self.evaluate_fcall(statement)
+            case _:
+                raise Exception(f"Invalid statement {statement.elem_type}")            
         
     def evaluate_expression(self, expression: Element):
-        # if this is a value node just return value
-        if expression.elem_type in Interpreter.VAL_NODES:
-            return expression.get("value")
-        
-        # if this is var node try to retrieve from scope
-        if expression.elem_type == InterpreterBase.VAR_NODE:
-            return self.scope.get_variable(expression.get("name"))
-        
-        # check that expression is a valid expression
-        assert(expression.elem_type in InterpreterBase.VALID_EXPRESSIONS)
-        
-        # if expression is a binary node
-        if expression.elem_type in Interpreter.BINARY_OP_NODES:
-            return self.evaluate_binary_op(expression)
-        elif expression.elem_type == InterpreterBase.FCALL_NODE:
-            return self.evaluate_fcall(expression)
-        
-        raise Exception(f"Invalid expression {expression.elem_type}")
+        match (expression.elem_type):
+            # if this is a value node just return value
+            case e_t if e_t in Interpreter.VAL_NODES:
+                return expression.get("value")
+            # if this is var node try to retrieve from scope
+            case InterpreterBase.VAR_NODE:
+                return self.scope.get_variable(expression.get("name"))
+            case e_t if e_t in Interpreter.BINARY_OP_NODES:
+                return self.evaluate_binary_op(expression)
+            case InterpreterBase.FCALL_NODE:
+                return self.evaluate_fcall(expression)
+            case _:
+                raise Exception(f"Invalid expression {expression.elem_type}")
         
     def evaluate_binary_op(self, binary_op: Element):
         left = self.evaluate_expression(binary_op.get("op1"))
         right = self.evaluate_expression(binary_op.get("op2"))
         
         match (binary_op.elem_type):
-            case InterpreterBase.ADD_NODE:
+            case Interpreter.ADD_NODE:
                 # check that these are both ints
                 if type(left) != int or type(right) != int:
-                    self.interpreter.raise_error(ErrorType.TYPE_ERROR, f"Invalid types for addition {type(left)} and {type(right)}")                
+                    self.interpreter.error(ErrorType.TYPE_ERROR, f"Invalid types for addition {type(left)} and {type(right)}")                
                 return self.evaluate_expression(left) + self.evaluate_expression(right)
-            case InterpreterBase.SUB_NODE:
+            case Interpreter.SUB_NODE:
                 # check that these are both ints
                 if type(left) != int or type(right) != int:
-                    self.interpreter.raise_error(ErrorType.TYPE_ERROR, f"Invalid types for subtraction {type(left)} and {type(right)}")    
+                    self.interpreter.error(ErrorType.TYPE_ERROR, f"Invalid types for subtraction {type(left)} and {type(right)}")    
                 return self.evaluate_expression(left) - self.evaluate_expression(right)
             case _:
                 # This should never happen, binary op is only called on operators belonging to BINARY_OP_NODES
                 raise Exception(f"Invalid binary operator {binary_op.elem_type}")
         
-        
+
+# ===================================== MAIN Testing =====================================
+def main():
+    # all programs will be provided to your interpreter as a python string, 
+    # just as shown here.
+    program_source = """func main() {
+        var x;
+        x = 5 + 6;
+        print("The sum is: ", x);
+    }
+    """
+    
+    # create an instance of your interpreter
+    interpreter = Interpreter()
+    
+    # run the interpreter
+    interpreter.run(program_source)
+    
+if __name__ == "__main__":
+    main()
